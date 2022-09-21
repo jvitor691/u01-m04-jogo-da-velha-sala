8365e1b33062b3d6ddb70dc787b5e845210364dc
prof-otavio-medeiros <alessandrofeitoza01@gmail.com>
2022-09-21 20:08:19 -0300

    commit 2022 09 21 001

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,29 @@
 let jogador = "X";
 
+function checkWinner() {
+    let elements = document.getElementById("container").children;
+    if(elements[0].innerText === elements[1].innerText &&
+        elements[1].innerText === elements[2].innerText && 
+        elements[2].innerText.length !== 0) {
+        debugger;
+    }
+    if(elements[3].innerText === elements[4].innerText &&
+        elements[4].innerText === elements[5].innerText && 
+        elements[5].innerText.length !== 0) {
+        debugger;
+    }
+}
+
 function mark(element) {
-    // element.innerText.length == 0 ? element.innerText = jogador : alert("Elemento já preenchido");
-    if(element.innerText.length == 0) {
+    if(element.innerText.length === 0) {
         element.innerText = jogador;
-        jogador == "X" ? jogador = "O" : jogador = "X";
+        // jogador == "X" ? jogador = "O" : jogador = "X";
+        if(jogador === "X") {
+            jogador = "O";
+        } else {
+            jogador = "X"
+        }
+        checkWinner();
     } else {
         alert("Elemento já preenchido")
     }    
