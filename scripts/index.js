b0450e56c421f9b3ef061868644dff95b1468d9c
Prof. Otávio Medeiros <prof.otavio.medeiros@gmail.com>
2022-09-24 10:35:51 -0300

    commit 2022 09 24 001

--- scripts/index.js
+++ scripts/index.js
@@ -1,30 +1,68 @@
 let jogador = "X";
 
 function checkWinner() {
-    let elements = document.getElementById("container").children;
-    if(elements[0].innerText === elements[1].innerText &&
-        elements[1].innerText === elements[2].innerText && 
-        elements[2].innerText.length !== 0) {
-        debugger;
-    }
-    if(elements[3].innerText === elements[4].innerText &&
-        elements[4].innerText === elements[5].innerText && 
-        elements[5].innerText.length !== 0) {
-        debugger;
-    }
+  let elements = document.getElementById("container").children;
+  if (
+    elements[0].innerText === elements[1].innerText &&
+    elements[1].innerText === elements[2].innerText &&
+    elements[2].innerText.length !== 0
+  ) {
+    debugger;
+  } else if (
+    elements[3].innerText === elements[4].innerText &&
+    elements[4].innerText === elements[5].innerText &&
+    elements[5].innerText.length !== 0
+  ) {
+    debugger;
+  } else if (
+    elements[6].innerText === elements[7].innerText &&
+    elements[7].innerText === elements[8].innerText &&
+    elements[8].innerText.length !== 0
+  ) {
+    debugger;
+  } else if (
+    elements[0].innerText === elements[3].innerText &&
+    elements[3].innerText === elements[6].innerText &&
+    elements[6].innerText.length !== 0
+  ) {
+    debugger;
+  } else if (
+    elements[1].innerText === elements[4].innerText &&
+    elements[4].innerText === elements[7].innerText &&
+    elements[7].innerText.length !== 0
+  ) {
+    debugger;
+  } else if (
+    elements[2].innerText === elements[5].innerText &&
+    elements[5].innerText === elements[8].innerText &&
+    elements[8].innerText.length !== 0
+  ) {
+    debugger;
+  } else if (
+    elements[0].innerText === elements[4].innerText &&
+    elements[4].innerText === elements[8].innerText &&
+    elements[8].innerText.length !== 0
+  ) {
+    debugger;
+  } else if (
+    elements[2].innerText === elements[4].innerText &&
+    elements[4].innerText === elements[6].innerText &&
+    elements[6].innerText.length !== 0
+  ) {
+    debugger;
+  }
 }
 
 function mark(element) {
-    if(element.innerText.length === 0) {
-        element.innerText = jogador;
-        // jogador == "X" ? jogador = "O" : jogador = "X";
-        if(jogador === "X") {
-            jogador = "O";
-        } else {
-            jogador = "X"
-        }
-        checkWinner();
+  if (element.innerText.length === 0) {
+    element.innerText = jogador;
+    if (jogador === "X") {
+      jogador = "O";
     } else {
-        alert("Elemento já preenchido")
-    }    
+      jogador = "X";
+    }
+    checkWinner();
+  } else {
+    alert("Elemento já preenchido");
+  }
 }
