6f83e9d8480aa9098876bbd6b4c030f273e468cc
prof-otavio-medeiros <alessandrofeitoza01@gmail.com>
2022-09-28 20:03:02 -0300

    commit 2022 09 28 001

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 let jogador = "X";
-
+ 
 function checkWinner() {
   let elements = document.getElementById("container").children;
   if (
@@ -15,57 +15,96 @@ function checkWinner() {
     elements[3].innerText === elements[4].innerText &&
     elements[4].innerText === elements[5].innerText &&
     elements[5].innerText.length !== 0
-  ) {
+  ) {    
+    elements[3].style="background-color: yellow;";
+    elements[4].style="background-color: yellow;";
+    elements[5].style="background-color: yellow;";
     return true;
   } else if (
     elements[6].innerText === elements[7].innerText &&
     elements[7].innerText === elements[8].innerText &&
     elements[8].innerText.length !== 0
   ) {
+    elements[6].style="background-color: yellow;";
+    elements[7].style="background-color: yellow;";
+    elements[8].style="background-color: yellow;";
     return true;
   } else if (
     elements[0].innerText === elements[3].innerText &&
     elements[3].innerText === elements[6].innerText &&
     elements[6].innerText.length !== 0
   ) {
+    elements[0].style="background-color: yellow;";
+    elements[3].style="background-color: yellow;";
+    elements[6].style="background-color: yellow;";
     return true;
   } else if (
     elements[1].innerText === elements[4].innerText &&
     elements[4].innerText === elements[7].innerText &&
     elements[7].innerText.length !== 0
   ) {
+    elements[1].style="background-color: yellow;";
+    elements[4].style="background-color: yellow;";
+    elements[7].style="background-color: yellow;";
     return true;
   } else if (
     elements[2].innerText === elements[5].innerText &&
     elements[5].innerText === elements[8].innerText &&
     elements[8].innerText.length !== 0
   ) {
+    elements[2].style="background-color: yellow;";
+    elements[5].style="background-color: yellow;";
+    elements[8].style="background-color: yellow;";
     return true;
   } else if (
     elements[0].innerText === elements[4].innerText &&
     elements[4].innerText === elements[8].innerText &&
     elements[8].innerText.length !== 0
   ) {
+    elements[0].style="background-color: yellow;";
+    elements[4].style="background-color: yellow;";
+    elements[8].style="background-color: yellow;";
     return true;
   } else if (
     elements[2].innerText === elements[4].innerText &&
     elements[4].innerText === elements[6].innerText &&
     elements[6].innerText.length !== 0
   ) {
+    elements[2].style="background-color: yellow;";
+    elements[4].style="background-color: yellow;";
+    elements[6].style="background-color: yellow;";
     return true;
   }
   return false;
 }
 
+function checkGameOver() {
+  let elements = document.getElementById("container").children;
+  let gameOver = true;
+  for(let indice = 0; indice < elements.length; indice++) {
+    if(elements[indice].innerText.length === 0) {
+      gameOver = false;
+    }
+  }
+  return gameOver;
+}
+
 let winner = false;
+let gameOver = false;
 
 function mark(element) {
-  if (!winner) {
+  if (!winner && !gameOver) {
     if (element.innerText.length === 0) {
       element.innerText = jogador;
       winner = checkWinner();
       if (winner) {
         document.getElementById("messages").innerText = "Vencedor: " + jogador;
+      } else {
+        gameOver = checkGameOver();
+        if(gameOver) {
+          document.getElementById("messages").innerText = "Fim de Jogo";
+          return;
+        }
       }
       if (jogador === "X") {
         jogador = "O";
---
+++ testes.js
@@ -0,0 +1,10 @@
+let nome1 = 'Otávio';
+let nome2 = 'Carla';
+let nome3 = 'José';
+
+let nome = ['Otávio', 'Carla', 'Marcus', 'Aurélio'];
+
+for (let indice = 0; indice < nome.length; indice++) {
+    console.log(nome[indice]);
+}
+
